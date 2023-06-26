f562b1af548efcd9ae63be840dee46e9d74eb803
Blaise Munyampirwa <blaise@groundlight.ai>
2023-06-26 11:40:57 -0700

    doc for AsyncMotionDetector

--- app/core/motion_detection.py
+++ app/core/motion_detection.py
@@ -6,6 +6,13 @@ from app.schemas.schemas import ImageQueryResponse
 
 
 class AsyncMotionDetector:
+    """Asynchronous motion detector.
+    This is a wrapper around MotionDetector that exposes an asynchronous
+    execution of motion_detected. Although this method need not be asynchronous
+    from a performance standpoint, we want it to be `async` since it will be
+    invoked in an asynchronous block.
+    """
+
     def __init__(self, percentage_threshold: float = 1, val_threshold: int = 50):
         self._detector = MotionDetector(pct_threshold=percentage_threshold, val_threshold=val_threshold)
         self._previous_image = None
