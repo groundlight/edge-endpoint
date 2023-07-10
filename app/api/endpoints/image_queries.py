8ada407e37bde4c5211c1616515219c16d660691
blaise <blaise@groundlight.ai>
2023-07-10 22:15:17 +0000

    fix endpoints

--- app/api/endpoints/image_queries.py
+++ app/api/endpoints/image_queries.py
@@ -2,6 +2,7 @@ import logging
 
 from fastapi import APIRouter, Depends
 from model import ImageQuery
+from fastapi import Query
 
 from app.core.utils import get_groundlight_instance
 from app.schemas.schemas import ImageQueryCreate
@@ -12,13 +13,23 @@ logger = logging.getLogger(__name__)
 router = APIRouter()
 
 
-@router.post("/submit", response_model=ImageQuery)
-async def post_image_query(props: ImageQueryCreate, gl: Depends = Depends(get_groundlight_instance)):
+@router.post("", response_model=ImageQuery)
+async def post_image_query(
+    props: ImageQueryCreate = Depends(ImageQueryCreate), gl: Depends = Depends(get_groundlight_instance)
+):
     """
     Submit an image query to the detector.
     """
-    detector_name = props.detector_name
+    image = props.image
+    detector_id = props.detector_id
     wait_time = props.wait
-    detector = gl.get_detector_by_name(name=detector_name)
-    image_query = gl.submit_image_query(detector=detector, image=props.image, wait=wait_time)
+    image_query = gl.submit_image_query(detector=detector_id, image=image, wait=wait_time)
     return image_query
+
+
+@router.get("", response_model=ImageQuery)
+async def get_image_query(props: str = Query(...), gl: Depends = Depends(get_groundlight_instance)):
+    """
+    Get an image query by ID.
+    """
+    return gl.get_image_query(image_query_id=props)
--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -4,9 +4,9 @@ import requests
 from pydantic import BaseModel, Field, confloat, validator
 
 
-class DetectorCreate(BaseModel):
+class DetectorCreateAndGet(BaseModel):
     name: str = Field(description="Name of the detector")
-    query: str = Field(description="Query associated with the detector")
+    query: Optional[str] = Field(description="Query associated with the detector")
     confidence_threshold: Optional[confloat(ge=0.0, le=1.0)] = Field(
         0.9,
         description=(
@@ -24,18 +24,19 @@ class ImageQueryCreate(BaseModel):
     Reference: https://fastapi.tiangolo.com/tutorial/encoder/
     """
 
-    detector_name: str = Field(description="Detector name")
+    detector_id: str = Field(description="Detector ID")
     image: Union[str, bytes] = Field(
         description="Image to submit to the detector. If the image is a string, it is assumed to be a URL."
     )
     wait: Optional[float] = Field(None, description="How long to wait for a confident response (seconds)")
 
     @validator("image")
+    @classmethod
     def validate_image(cls, value):
         return cls._sanitize_image_input(image=value)
 
-    @staticmethod
-    def _sanitize_image_input(image: Union[str, bytes]) -> bytes:
+    @classmethod
+    def _sanitize_image_input(cls, image: Union[str, bytes]) -> bytes:
         """Sanitizes the image input to be a bytes object.
 
         Args:
@@ -47,7 +48,7 @@ class ImageQueryCreate(BaseModel):
         Returns:
             bytes: Image bytes.
         """
-        if isinstance(image, str):
+        if isinstance(image, str) and image.startswith("http"):
             # If the image is a URL, get the image bytes
             return requests.get(image, timeout=5).content
         elif isinstance(image, bytes):
