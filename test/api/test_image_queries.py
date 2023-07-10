c667e4bd8f99afb38e8fec9db36eb1a8127a9f63
blaise <blaise@groundlight.ai>
2023-07-10 22:29:13 +0000

    pydantic

--- test/api/test_image_queries.py
+++ test/api/test_image_queries.py
@@ -8,11 +8,11 @@ from ..parsing import parse
 
 def test_post_image_queries(test_client: TestClient):
     # TODO: improve this test
-    detector_name = "edge-testing"
+    detector_id = "det_2SLWms1fkTuc3KMedLEQ0HVYsF0"
     image = "https://www.photos-public-domain.com/wp-content/uploads/2010/11/over_flowing_garbage_can.jpg"
 
     url = full_path(IMAGE_QUERIES)
-    body = ImageQueryCreate(detector_name=detector_name, image=image, wait=20).dict()
+    body = ImageQueryCreate(detector_id=detector_id, image=image, wait=20).dict()
     # NOTE: bytes are not JSON-serializable, so this is commented out for now
     # response = test_client.post(url, json=body)
 
