43fb0e906c44cdadab0433c0e26f7503cb6d5709
blaise-muhirwa <blaise@groundlight.ai>
2023-07-11 21:20:18 -0700

    initial commit

--- app/core/motion_detection.py
+++ app/core/motion_detection.py
@@ -5,6 +5,7 @@ import numpy as np
 from framegrab import MotionDetector
 from model import ImageQuery
 from pydantic import BaseSettings, Field
+import faiss 
 
 
 class MotdetParameterSettings(BaseSettings):
@@ -27,13 +28,14 @@ class AsyncMotionDetector:
     invoked asynchronously from the API.
     """
 
-    def __init__(self, parameters: MotdetParameterSettings):
+    def __init__(self, parameters: MotdetParameterSettings, image_dim: int):
         self._motion_detector = MotionDetector(
             pct_threshold=parameters.motdet_percentage_threshold, val_threshold=parameters.motdet_val_threshold
         )
         self._previous_image = None
         self.lock = Lock()
         self._image_query_response = None
+        self._index = faiss.IndexFlatL2(image_dim)
 
     @property
     def image_query_response(self):
@@ -42,6 +44,13 @@ class AsyncMotionDetector:
     @image_query_response.setter
     def image_query_response(self, response: ImageQuery):
         self._image_query_response = response
+        
+    def add_image(self, image: np.ndarray) -> None:
+        self._index.add(image)
+        
+    def search_image(self, image: np.ndarray, k: int = 1) -> np.ndarray:
+        _ , neighbors = self._index.search(image, k)
+        return neighbors
 
     async def motion_detected(self, new_img: np.ndarray) -> bool:
         return await asyncio.to_thread(self._motion_detector.motion_detected, new_img)
