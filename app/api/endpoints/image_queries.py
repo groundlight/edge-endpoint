f439791f509bf29a62e74625e2dbf7243de22389
blaise-muhirwa <blaise@groundlight.ai>
2023-07-19 16:57:09 -0700

    fix post_image_query endpoint

--- app/api/endpoints/image_queries.py
+++ app/api/endpoints/image_queries.py
@@ -3,12 +3,11 @@ from io import BytesIO
 from typing import Optional
 
 import numpy as np
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, Query, Request
 from model import ImageQuery
 from PIL import Image
 
 from app.core.utils import get_groundlight_sdk_instance, get_motion_detector_instance, prefixed_ksuid
-from app.schemas.schemas import ImageQueryCreate
 
 logger = logging.getLogger(__name__)
 
@@ -18,19 +17,13 @@ router = APIRouter()
 
 @router.post("", response_model=ImageQuery)
 async def post_image_query(
-    props: ImageQueryCreate = Depends(ImageQueryCreate),
+    detector_id: str,
+    wait: float = None,
+    request: Request = None,
     gl: Depends = Depends(get_groundlight_sdk_instance),
     motion_detector: Depends = Depends(get_motion_detector_instance),
 ):
-    """
-    Submit an image query to the detector.
-    NOTE: For now motion detection assumes that images are submitted to the
-    same detector. If the client sends the same image to multiple detectors
-    we would incorrectly flag no motion detected for the second detector.
-    """
-    image = props.image
-    detector_id = props.detector_id
-    wait_time = props.wait
+    image = await request.body()
     img = Image.open(BytesIO(image))
     img_numpy = np.array(img)
 
@@ -43,19 +36,14 @@ async def post_image_query(
         )
 
         if motion_detected or iq_response_is_improvable:
-            image_query = gl.submit_image_query(detector=detector_id, image=image, wait=wait_time)
-
-            # Store the cloud's response so that if the next image has no motion, we will return
-            # the same response.
+            image_query = gl.submit_image_query(detector=detector_id, image=image, wait=wait)
             motion_detector.image_query_response = image_query
             logger.debug("Motion detected")
             return image_query
 
     logger.debug("No motion detected")
-
     new_image_query = ImageQuery(**motion_detector.image_query_response.dict())
     new_image_query.id = prefixed_ksuid(prefix="iqe_")
-
     return new_image_query
 
 
