910319300baeeedece82580aacd1d72f4150b972
blaise-muhirwa <blaise@groundlight.ai>
2023-08-16 16:45:10 -0700

    remove debugging code

--- app/main.py
+++ app/main.py
@@ -12,7 +12,6 @@ from .core.motion_detection import AsyncMotionDetector, MotdetParameterSettings
 LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO").upper()
 
 logging.basicConfig(level=LOG_LEVEL)
-logger = logging.getLogger(__name__)
 
 app = FastAPI()
 app.include_router(router=api_router, prefix=API_BASE_PATH)
@@ -23,9 +22,6 @@ app.include_router(router=ping_router)
 app.state.groundlight = Groundlight()
 
 parameters = MotdetParameterSettings()
-
-logger.debug(f"Motion detection parameters: {parameters}")
-logger.debug(f"type of motdet-enabled: {type(parameters.motion_detection_enabled)}")
 
 # Create global shared motion detector object in the app's state
 app.state.motion_detector = AsyncMotionDetector(parameters=MotdetParameterSettings())
