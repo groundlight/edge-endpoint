5d6050c88896c464b51bfaac306a9638be1eac24
blaise-muhirwa <blaise@groundlight.ai>
2023-08-18 14:32:13 -0700

    update SDK version

--- app/api/routes/image_queries.py
+++ app/api/routes/image_queries.py
@@ -1,8 +1,9 @@
 import logging
 from io import BytesIO
+from typing import Optional
 
 import numpy as np
-from fastapi import APIRouter, Depends, HTTPException, Request
+from fastapi import APIRouter, Depends, HTTPException, Request, Query
 from model import ImageQuery
 from PIL import Image, ImageFile
 
@@ -24,8 +25,8 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 @router.post("", response_model=ImageQuery)
 async def post_image_query(
-    detector_id: str,
-    wait: float = None,
+    detector_id: str = Query(..., description="Detector ID"),
+    patience_time: Optional[float] = Query(None, description="How long to wait for a confident response"),
     request: Request = None,
     gl: Depends = Depends(get_groundlight_sdk_instance),
     motion_detector: Depends = Depends(get_motion_detector_instance),
@@ -36,13 +37,13 @@ async def post_image_query(
     img_numpy = np.array(img)
 
     if not motion_detector.is_enabled():
-        return safe_call_api(gl.submit_image_query, detector=detector_id, image=image, wait=wait)
+        return safe_call_api(gl.submit_image_query, detector=detector_id, image=image, wait=patience_time)
 
     async with motion_detector.lock:
         motion_detected = await motion_detector.motion_detected(new_img=img_numpy)
 
         if motion_detected:
-            image_query = safe_call_api(gl.submit_image_query, detector=detector_id, image=image, wait=wait)
+            image_query = safe_call_api(gl.submit_image_query, detector=detector_id, image=image, wait=patience_time)
             # Store the cloud's response so that if the next image has no motion, we will return
             # the same response
             motion_detector.image_query_response = image_query
