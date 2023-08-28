e14cb574ccf9e7464bd20d472c17310e68fa4886
Tyler Romero <tyler@groundlight.ai>
2023-08-28 13:02:28 -0700

    Address code review comments

--- deploy/make-aws-secret.sh
+++ deploy/make-aws-secret.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -ex
+
+K="k3s kubectl"
 
 # Enable ECR login - make sure you have the aws client configured properly, or an IAM role
 # attached to your instance
@@ -11,10 +14,10 @@ aws ecr get-login-password --region us-west-2 | docker login \
 
 # Create an AWS secret for the edge-endpoint to properly pull images from ECR
 # Note: needs testing
-k3s kubectl delete --ignore-not-found secret registry-credentials
+$K delete --ignore-not-found secret registry-credentials
 
 PASSWORD=$(aws ecr get-login-password --region us-west-2)
-k3s kubectl create secret docker-registry registry-credentials \
+$K create secret docker-registry registry-credentials \
     --docker-server=723181461334.dkr.ecr.us-west-2.amazonaws.com \
     --docker-username=AWS \
     --docker-password=$PASSWORD
--- deploy/make-gl-api-token-secret.sh
+++ deploy/make-gl-api-token-secret.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+set -ex
+
+K="k3s kubectl"
+
 # Create a kubernetes secret for the groundlight api token
 # Make sure that you have the groundlight api token set in your environment
 
-k3s kubectl create secret generic groundlight-secrets \
+$K create secret generic groundlight-secrets \
     --from-literal=api-token=${GROUNDLIGHT_API_TOKEN}
