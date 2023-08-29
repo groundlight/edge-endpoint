199323322eb382341cda7c9d509c75dbdf12619c
Tyler Romero <tyler@groundlight.ai>
2023-08-29 12:34:42 -0700

    Add docstrings

--- deploy/push-edge-endpoint-image.sh
+++ deploy/push-edge-endpoint-image.sh
@@ -6,7 +6,7 @@ set -ex
 # Ensure that you're in the same directory as this script before running it
 cd "$(dirname "$0")"
 
-TAG=$(./git-tag-name)
+TAG=$(./git-tag-name.sh)
 
 # Authenticate docker to ECR
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin  723181461334.dkr.ecr.us-west-2.amazonaws.com
