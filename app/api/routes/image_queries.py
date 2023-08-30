a0d3b5850ef04c93b5541590d578a4fb7fc7c03d
blaise-muhirwa <135643310+blaise-muhirwa@users.noreply.github.com>
2023-08-29 18:59:29 -0700

    [small] validate request body in post image query (#26)
    * validate request body
    
    * address pr feedback
    
    * tests
    
    * Update app/api/routes/image_queries.py
    
    Co-authored-by: robotrapta <79607467+robotrapta@users.noreply.github.com>
    
    * Automatically reformatting code with black and isort
    
    * refactoring
    
    ---------
    
    Co-authored-by: robotrapta <79607467+robotrapta@users.noreply.github.com>
    Co-authored-by: Auto-format Bot <autoformatbot@groundlight.ai>

--- app/api/routes/image_queries.py
+++ app/api/routes/image_queries.py
@@ -5,7 +5,7 @@ from typing import Optional
 import numpy as np
 from fastapi import APIRouter, Depends, HTTPException, Query, Request
 from model import ImageQuery
-from PIL import Image, ImageFile
+from PIL import Image
 
 from app.core.utils import (
     get_edge_detector_manager,
@@ -20,29 +20,45 @@ logger = logging.getLogger(__name__)
 
 router = APIRouter()
 
-ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+async def validate_request_body(request: Request) -> Image.Image:
+    if not request.headers.get("Content-Type", "").startswith("image/"):
+        raise HTTPException(status_code=400, detail="Request body must be image bytes")
+
+    image_bytes = await request.body()
+    try:
+        # Attempt to open the image
+        image = Image.open(BytesIO(image_bytes))
+
+        # Image.open() does not fully process the image data. It's possible for Image.open()
+        # to succeed but then fail when the image data is actually being processed.
+        # To ensure that the image can be fully processed, we call img.load() to force loading
+        # the entire image. If this fails, we know that the image is invalid.
+
+        image.load()
+        return image
+    except Exception:
+        logger.error("Failed to load image", exc_info=True)
+        raise HTTPException(status_code=400, detail="Invalid input image")
 
 
 @router.post("", response_model=ImageQuery)
 async def post_image_query(
     detector_id: str = Query(..., description="Detector ID"),
     patience_time: Optional[float] = Query(None, description="How long to wait for a confident response"),
-    request: Request = None,
+    img: Image.Image = Depends(validate_request_body),
     gl: Depends = Depends(get_groundlight_sdk_instance),
     motion_detector: Depends = Depends(get_motion_detector_instance),
     edge_detector_manager: Depends = Depends(get_edge_detector_manager),
 ):
-    image = await request.body()
-    img = Image.open(BytesIO(image))
-    img_numpy = np.array(img)
-
     if not motion_detector.is_enabled():
-        return safe_call_api(gl.submit_image_query, detector=detector_id, image=image, wait=patience_time)
+        return safe_call_api(gl.submit_image_query, detector=detector_id, image=img, wait=patience_time)
 
+    img_numpy = np.array(img)
     motion_detected = motion_detector.motion_detected(new_img=img_numpy)
 
     if motion_detected:
-        image_query = safe_call_api(gl.submit_image_query, detector=detector_id, image=image, wait=patience_time)
+        image_query = safe_call_api(gl.submit_image_query, detector=detector_id, image=img, wait=patience_time)
         # Store the cloud's response so that if the next image has no motion, we will return
         # the same response
         motion_detector.image_query_response = image_query
