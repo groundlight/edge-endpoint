04d2254f6307361e976b35f832c9c499949c7068
blaise-muhirwa <blaise@groundlight.ai>
2023-09-12 15:56:20 -0700

    add comment

--- app/core/motion_detection.py
+++ app/core/motion_detection.py
@@ -68,12 +68,16 @@ class MotionDetectorWrapper:
         Indicates if the unconfident image query re-escalation interval has been exceeded.
         If the old image query still has low confidence, and it's been more than
         `unconfident_iq_reescalation_interval` seconds, we pretend we have motion.
+        This is to force the cloud to "think harder" about images which the customer is still seeing
         """
-        current_time = time.monotonic()
-        if current_time - self._previous_motion_detection_time > self._unconfident_iq_reescalation_interval:
-            self._previous_motion_detection_time = current_time
-            logger.debug("Unconfident image query re-escalation interval exceeded")
-            return True
+
+        if self._previous_motion_detection_time is not None:
+            current_time = time.monotonic()
+            if current_time - self._previous_motion_detection_time > self._unconfident_iq_reescalation_interval:
+                self._previous_motion_detection_time = current_time
+                logger.debug("Unconfident image query re-escalation interval exceeded")
+                return True
+
         return False
 
     def enable(self) -> None:
