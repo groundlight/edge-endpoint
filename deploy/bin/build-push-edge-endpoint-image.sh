e1e839c412df334735d64a42a3a5a20fadc473e4
Tyler Romero <tyler@groundlight.ai>
2023-09-14 10:15:55 -0700

    better error handling

--- deploy/bin/build-push-edge-endpoint-image.sh
+++ deploy/bin/build-push-edge-endpoint-image.sh
@@ -11,8 +11,22 @@ TAG=$(./git-tag-name.sh)
 # Authenticate docker to ECR
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin  723181461334.dkr.ecr.us-west-2.amazonaws.com
 
-# Prep for multiplatform build - the build is done INSIDE a docker container
-docker buildx create --name tempbuilder --use
+# We use docker buildx to build the image for multiple platforms. buildx comes
+# installed with Docker Engine when installed via Docker Desktop. If you're
+# on a Linux machine with an old version of Docker Engine, you may need to
+# install buildx manually. Follow these instructions to install docker-buildx-plugin:
+# https://docs.docker.com/engine/install/ubuntu/
+
+# Check if tempbuilder already exists
+if ! docker buildx ls | grep -q tempbuilder; then
+  # Prep for multiplatform build - the build is done INSIDE a docker container
+  docker buildx create --name tempbuilder --use
+else
+  # If tempbuilder exists, set it as the current builder
+  docker buildx use tempbuilder
+fi
+
+# Ensure that the tempbuilder container is running
 docker buildx inspect tempbuilder --bootstrap
 
 # Build image for amd64 and arm64
