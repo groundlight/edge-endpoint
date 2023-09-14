095de53a2581afffe447b0b510247f6d36924331
Tyler Romero <tyler@groundlight.ai>
2023-09-13 20:29:14 -0700

    Support building image on both arm64 and amd64

--- deploy/bin/build-push-edge-endpoint-image.sh
+++ deploy/bin/build-push-edge-endpoint-image.sh
@@ -11,11 +11,18 @@ TAG=$(./git-tag-name.sh)
 # Authenticate docker to ECR
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin  723181461334.dkr.ecr.us-west-2.amazonaws.com
 
-# Build image
-docker build --target production-image --tag edge-endpoint ../..
+# Prep for multiplatform build - the build is done INSIDE a docker container
+docker buildx create --name tempbuilder --use
+docker buildx inspect tempbuilder --bootstrap
+
+# Build image for amd64 and arm64
+docker buildx build --platform linux/amd64,linux/arm64 --target production-image --tag edge-endpoint ../..
 
 # Tag image
 docker tag edge-endpoint:latest 723181461334.dkr.ecr.us-west-2.amazonaws.com/edge-endpoint:${TAG}
 
 # Push image to ECR
 docker push 723181461334.dkr.ecr.us-west-2.amazonaws.com/edge-endpoint:${TAG}
+
+# Cleanup
+docker buildx rm tempbuilder
