643d5a39142f9c54e64dd96ab800b7bffc648e8c
blaise-muhirwa <blaise@groundlight.ai>
2023-09-25 17:58:12 +0000

    addressing pr feedback

--- app/core/app_state.py
+++ app/core/app_state.py
@@ -76,8 +76,8 @@ def get_detector_metadata(detector_id: str, gl: Groundlight) -> Detector:
 
 
 class AppState:
-    # TTL cache for k3s health checks on inference deployments. Checks are cached for 10 minutes.
-    KUBERNETES_HEALTH_CHECKS_TTL_CACHE = TTLCache(maxsize=MAX_DETECTOR_IDS_TTL_CACHE_SIZE, ttl=600)
+    # TTL cache for k3s health checks on inference deployments. Checks are cached for 1 minutes.
+    KUBERNETES_HEALTH_CHECKS_TTL_CACHE = TTLCache(maxsize=MAX_DETECTOR_IDS_TTL_CACHE_SIZE, ttl=60)
 
     def __init__(self):
         # Create a global shared image query ID cache in the app's state
@@ -119,33 +119,33 @@ class AppState:
         # The `AppsV1Api` client manages resources related to workloads, such as Deployments, StatefulSets, etc.
         # The `CoreV1Api` client, on the other hand, handles core cluster resources like Pods, Services, and Namespaces.
         # Using both clients in order to create the deployment and service for the inference container.
-        self.app_kube_client = kube_client.AppsV1Api()
-        self.core_kube_client = kube_client.CoreV1Api()
+        self._app_kube_client = kube_client.AppsV1Api()
+        self._core_kube_client = kube_client.CoreV1Api()
 
-        self._edge_deployment_template = self._load_k3s_edge_deployment_manifest()
+        self._inference_deployment_template = self._load_inference_deployment_template()
 
-    def _load_k3s_edge_deployment_manifest(self) -> str:
+    def _load_inference_deployment_template(self) -> str:
         """
-        Loads the k3s edge deployment manifest template.
+        Loads the inference deployment template.
         """
-        deployment_manifest_path = "/etc/groundlight/inference_deployment.yaml"
-        if os.path.exists(deployment_manifest_path):
-            with open(deployment_manifest_path, "r") as f:
+        deployment_template_path = "/etc/groundlight/inference_deployment.yaml"
+        if os.path.exists(deployment_template_path):
+            with open(deployment_template_path, "r") as f:
                 manifest = f.read()
                 return manifest
 
-        raise FileNotFoundError(f"Could not find k3s edge deployment manifest at {deployment_manifest_path}")
+        raise FileNotFoundError(f"Could not find k3s inference deployment template at {deployment_template_path}")
 
-    def _apply_kube_manifest(self, k3s_namespace, manifest) -> None:
+    def _apply_kube_manifest(self, namespace: str, manifest: str) -> None:
         """
-        Applies the kubernetes manifest to the kubernetes cluster.
+        Applies manifest to the kubernetes cluster.
         """
         for document in yaml.safe_load_all(manifest):
             try:
                 if document["kind"] == "Service":
-                    self.core_kube_client.create_namespaced_service(namespace=k3s_namespace, body=document)
+                    self._core_kube_client.create_namespaced_service(namespace=namespace, body=document)
                 elif document["kind"] == "Deployment":
-                    self.app_kube_client.create_namespaced_deployment(namespace=k3s_namespace, body=document)
+                    self._app_kube_client.create_namespaced_deployment(namespace=namespace, body=document)
                 else:
                     raise ValueError(f"Unknown kubernetes manifest kind: {document['kind']}")
 
@@ -154,25 +154,25 @@ class AppState:
                 # Currently not raising the exception (which from the app's perspective is a 500 error) so that
                 # we can continue to serve requests through the cloud API even if a specific inference
                 # deployment creation fails.
-                logger.warning(f"Failed to create a kubernetes deployment: {e}", exc_info=True)
+                logger.error(f"Failed to create a kubernetes deployment: {e}", exc_info=True)
 
-        logger.debug(f"Successfully applied kubernetes manifest to namespace `{k3s_namespace}`")
+        logger.debug(f"Applying kubernetes manifest to namespace `{namespace}`...")
 
     @cachetools.cached(
         cache=KUBERNETES_HEALTH_CHECKS_TTL_CACHE,
         key=lambda self, detector_id, *args, **kwargs: detector_id,
     )
     def inference_deployment_is_ready(
-        self, detector_id: str, k3s_namespace: str = "default", create_if_absent: bool = False
+        self, detector_id: str, namespace: str = "default", create_if_absent: bool = True
     ) -> bool:
         """
-        Checks if the detector deployment exists, and if not, creates it.
-        Running self.app_kube_client.create_namespaced_deployment() is equivalent to
-        running `k3s kubectl apply -f <deployment-manifest-yaml> -n <namespace>`
+        Checks if the detector's inference deployment exists and is ready, and optionally creates it.
+        Running self._app_kube_client.create_namespaced_deployment() is equivalent to
+        running `kubectl apply -f <deployment-manifest-yaml> -n <namespace>`
         to create the deployment.
 
         :param detector_id: ID of the detector on which to run local edge inference
-        :param k3s_namespace: Namespace in which to create the deployment
+        :param namespace: Namespace in which to create the deployment
         :param create_if_absent: Whether to create the deployment if it doesn't exist
         """
 
@@ -195,22 +195,27 @@ class AppState:
         service_name, deployment_name = get_service_and_deployment_names(detector_id=detector_id)
 
         try:
-            deployment = self.app_kube_client.read_namespaced_deployment(name=deployment_name, namespace=k3s_namespace)
+            deployment = self._app_kube_client.read_namespaced_deployment(name=deployment_name, namespace=namespace)
             if deployment.status.ready_replicas == deployment.spec.replicas:
                 logger.debug(f"Deployment {deployment_name} is ready")
 
                 # Check that the model in the deployment is also ready to server inference requests
                 return self.edge_inference_manager.inference_is_available(detector_id=detector_id)
 
-        except kube_client.rest.ApiException:
-            logger.debug(f"Deployment {deployment_name} does not currently exist in namespace {k3s_namespace}.")
-            if create_if_absent:
-                logger.debug(f"Creating deployment {deployment_name} in namespace {k3s_namespace}")
-                deployment_manifest = self._edge_deployment_template
-                deployment_manifest = deployment_manifest.replace("{{ INFERENCE_SERVICE_NAME }}", service_name)
-                deployment_manifest = deployment_manifest.replace("{{ DEPLOYMENT_NAME }}", deployment_name)
+        except kube_client.rest.ApiException as e:
+            if e.status == 404:
+                logger.debug(f"Deployment {deployment_name} does not currently exist in namespace {namespace}.")
 
-                self._apply_kube_manifest(k3s_namespace=k3s_namespace, manifest=deployment_manifest)
+                if create_if_absent:
+                    logger.info(f"Creating deployment {deployment_name} in namespace {namespace}")
+                    deployment_manifest = self._inference_deployment_template
+                    deployment_manifest = deployment_manifest.replace("{{ INFERENCE_SERVICE_NAME }}", service_name)
+                    deployment_manifest = deployment_manifest.replace("{{ DEPLOYMENT_NAME }}", deployment_name)
+
+                    self._apply_kube_manifest(namespace=namespace, manifest=deployment_manifest)
+
+            else:
+                logger.error(f"Failed to read deployment {deployment_name}: {e}", exc_info=True)
 
         return False
 
