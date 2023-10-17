9622773d5b8c46ea1245964410f4b92e2a8ed08d
blaise-muhirwa <blaise@groundlight.ai>
2023-10-17 22:03:19 +0000

    wip

--- certificates/generate_tls_cert.sh
+++ certificates/generate_tls_cert.sh
@@ -9,7 +9,7 @@ sudo openssl genpkey -algorithm Ed25519 -out ${TLS_CERT_DIR}/nginx_ed25519.key
 
 # Generate a self-signed certificate using the Ed25519 Private key
 # Valid for 365 days
-sudo openssl req -x509 -new \
+sudo openssl req -trustout -x509 -new \
         -key ${TLS_CERT_DIR}/nginx_ed25519.key \
         -out ${TLS_CERT_DIR}/nginx_ed25519.crt \
         -days 365 
