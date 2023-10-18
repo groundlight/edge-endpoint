de7c00033758bdb71c52563ac12bb7ddfabf1b48
blaise-muhirwa <blaise@groundlight.ai>
2023-10-18 20:57:28 +0000

    add openssl conf file

--- certificates/generate_tls_cert.sh
+++ certificates/generate_tls_cert.sh
@@ -2,6 +2,9 @@
 
 set -ex 
 
+# Change to current directory 
+cd $(dirname $0)
+
 TLS_CERT_DIR=/etc/nginx/ssl
 
 # Generate an Ed25519 Private key 
@@ -9,7 +12,9 @@ sudo openssl genpkey -algorithm Ed25519 -out ${TLS_CERT_DIR}/nginx_ed25519.key
 
 # Generate a self-signed certificate using the Ed25519 Private key
 # Valid for 365 days
-sudo openssl req -trustout -x509 -new \
+sudo openssl req -new -x509 \
+        -config openssl-custom.cnf \
+        -batch \
         -key ${TLS_CERT_DIR}/nginx_ed25519.key \
         -out ${TLS_CERT_DIR}/nginx_ed25519.crt \
         -days 365 
