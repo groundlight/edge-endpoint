e07cdfc88e46aa71749cbe91334c538855f3185c
Harry Tung <honeytung@gmail.com>
2024-07-01 14:15:30 -0700

    Updated inference threshold

--- test/api/test_motdet.py
+++ test/api/test_motdet.py
@@ -257,7 +257,7 @@ def test_motion_detection_not_sufficient_if_doesnt_meet_conf_threshold(gl: Groun
         detector=detector.id,
         image=original_image,
         wait=10,
-        confidence_threshold=base_iq_response.result.confidence + 1e-3,  # Require a higher confidence than before
+        confidence_threshold= 1.0,  # Require a higher confidence than before to trigger cloud inference
     )
 
     assert new_response.id != base_iq_response.id, "ImageQuery id should be different whether or not motion det is run"
