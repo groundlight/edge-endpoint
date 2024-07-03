12a31d37e789a447ea8d8e23fa686bcc08948c2a
Harry Tung <honeytung@gmail.com>
2024-07-03 13:30:42 -0700

    Updated motion detection test

--- test/api/test_motdet.py
+++ test/api/test_motdet.py
@@ -239,15 +239,15 @@ def test_motion_detection_not_sufficient_if_doesnt_meet_conf_threshold(gl: Groun
 
     detector_id = DETECTORS["dog_detector"]["detector_id"]
     detector = gl.get_detector(id=detector_id)
+    
+    # Set detector confidence threshold to 0.90
+    gl.update_detector_confidence_threshold(detector.id, 0.90)
 
     original_image = Image.open("test/assets/dog.jpeg")
-    
-    # Set wait time to be 30s
-    wait_time = 30.0
 
     # Set up opportunity for motion detection
     base_iq_response = gl.submit_image_query(
-        detector=detector.id, image=original_image, wait=wait_time, confidence_threshold=0.5
+        detector=detector.id, image=original_image, confidence_threshold=0.5
     )
     if (
         base_iq_response.result is None
@@ -255,16 +255,31 @@ def test_motion_detection_not_sufficient_if_doesnt_meet_conf_threshold(gl: Groun
         or base_iq_response.result.confidence == 1.0
     ):
         pytest.skip("This test requires that the cached image query response has a confidence < 1.0")
+    
+    # Update detector confidence threshold to 0.95
+    gl.update_detector_confidence_threshold(detector.id, 0.95)
 
     new_response = gl.submit_image_query(
         detector=detector.id,
         image=original_image,
-        wait=wait_time,
         confidence_threshold=base_iq_response.result.confidence + 1e-3,  # Require a higher confidence than before
     )
 
     assert new_response.id != base_iq_response.id, "ImageQuery id should be different whether or not motion det is run"
+    
+    if (
+        new_response.result is None
+        or new_response.result.confidence is None
+        or new_response.result.confidence < 0.95
+    ):
+        # Revert the confidence threshold to 0.90
+        gl.update_detector_confidence_threshold(detector.id, 0.90)
+        pytest.skip("This test requires that the cached image query response has a confidence < 0.95")
+        
     assert new_response.id.startswith("iq_"), (
         "ImageQuery id should start with 'iq_' because it was created on the cloud, because the cached mot det "
         "response did not meet the confidence threshold"
     )
+    
+    # Revert the confidence threshold to 0.90
+    gl.update_detector_confidence_threshold(detector.id, 0.90)
