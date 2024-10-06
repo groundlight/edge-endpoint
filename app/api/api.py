1d856308b57809fe25ccbb655bdd0a344841b6d2
Tyler Romero <tyler@groundlight.ai>
2024-10-05 22:44:36 -0700

    Remove detectors endpoints

--- app/api/api.py
+++ app/api/api.py
@@ -7,10 +7,6 @@ IMAGE_QUERIES = "image-queries"
 IMAGE_QUERIES_PREFIX = path_prefix(IMAGE_QUERIES)
 IMAGE_QUERIES_TAG = tag(IMAGE_QUERIES)
 
-DETECTORS = "detectors"
-DETECTORS_PREFIX = path_prefix(DETECTORS)
-DETECTORS_TAG = tag(DETECTORS)
-
 HEALTH = "health"
 HEALTH_PREFIX = path_prefix(HEALTH)
 HEALTH_TAG = tag(HEALTH)
@@ -21,7 +17,6 @@ PING_TAG = tag(PING)
 
 api_router = APIRouter()
 api_router.include_router(image_queries.router, prefix=IMAGE_QUERIES_PREFIX, tags=[IMAGE_QUERIES_TAG])
-# api_router.include_router(detectors.router, prefix=DETECTORS_PREFIX, tags=[DETECTORS_TAG])
 
 ping_router = APIRouter()
 ping_router.include_router(ping.router, prefix=PING_PREFIX, tags=[PING_TAG])
