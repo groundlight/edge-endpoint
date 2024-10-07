a77c6fbfd018bcbf03b68884294e6b451b0c3d98
Tyler Romero <tyler@groundlight.ai>
2024-10-07 15:04:53 -0700

    Revert "Reinstate the detectors endpoints"
    This reverts commit a95c4d0693191ba63ff4b99f57e88b3d7baeca6a.

--- app/api/api.py
+++ app/api/api.py
@@ -1,15 +1,11 @@
 from fastapi import APIRouter
 
 from app.api.naming import path_prefix, tag
-from app.api.routes import detectors, health, image_queries, ping
+from app.api.routes import health, image_queries, ping
 
 IMAGE_QUERIES = "image-queries"
 IMAGE_QUERIES_PREFIX = path_prefix(IMAGE_QUERIES)
 IMAGE_QUERIES_TAG = tag(IMAGE_QUERIES)
-
-DETECTORS = "detectors"
-DETECTORS_PREFIX = path_prefix(DETECTORS)
-DETECTORS_TAG = tag(DETECTORS)
 
 HEALTH = "health"
 HEALTH_PREFIX = path_prefix(HEALTH)
@@ -21,7 +17,6 @@ PING_TAG = tag(PING)
 
 api_router = APIRouter()
 api_router.include_router(image_queries.router, prefix=IMAGE_QUERIES_PREFIX, tags=[IMAGE_QUERIES_TAG])
-api_router.include_router(detectors.router, prefix=DETECTORS_PREFIX, tags=[DETECTORS_TAG])
 
 ping_router = APIRouter()
 ping_router.include_router(ping.router, prefix=PING_PREFIX, tags=[PING_TAG])
