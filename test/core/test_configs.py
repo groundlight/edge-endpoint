c7d92aef9633d020dabf3c1eb913dc42a2a089b5
Tyler Romero <tyler@groundlight.ai>
2024-10-31 13:45:55 -0700

    Fix tests

--- test/core/test_configs.py
+++ test/core/test_configs.py
@@ -57,23 +57,3 @@ def test_detector_config_validation():
             always_return_edge_prediction=False,
             disable_cloud_escalation=True,
         )
-
-    # min_time_between_escalations cannot be set if always_return_edge_prediction is False
-    with pytest.raises(ValueError):
-        DetectorConfig(
-            detector_id="det_xyz",
-            local_inference_template="default",
-            always_return_edge_prediction=False,
-            disable_cloud_escalation=False,
-            min_time_between_escalations=10,
-        )
-
-    # min_time_between_escalations cannot be set if disable_cloud_escalation is True
-    with pytest.raises(ValueError):
-        DetectorConfig(
-            detector_id="det_xyz",
-            local_inference_template="default",
-            always_return_edge_prediction=True,
-            disable_cloud_escalation=True,
-            min_time_between_escalations=10,
-        )
