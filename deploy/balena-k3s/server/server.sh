59fd4e6d864f590f9354a92fc6896f1b8dc9cbf8
Tyler Romero <tyler@groundlight.ai>
2024-11-13 18:03:26 -0800

    everything working at this point

--- deploy/balena-k3s/server/server.sh
+++ deploy/balena-k3s/server/server.sh
@@ -2,11 +2,24 @@
 
 set -eux
 
+# Kill any existing k3s server processes
+# WARNING: This will delete all cluster data!
+pkill -f k3s || true
+rm -rf /var/lib/rancher/k3s/*
+sleep 2
+
 # https://docs.k3s.io/cli/server
 # https://docs.k3s.io/datastore/ha-embedded
 # https://github.com/balena-io-experimental/balena-k3s/blob/main/server/server.sh
 
-# Copy our manifests to the K3s manifests directory
+mount --make-rshared /
+
+# Restart containerd, since `nvidia-ctk runtime configure --runtime=containerd` will have modified the containerd config
+# systemctl restart containerd
+
+# Copy our manifests to the K3s manifests directory. Any manifests in this directory will be automatically applied
+# to the cluster by K3s. This is how we deploy the NVIDIA GPU operator, among other things.
+# https://docs.k3s.io/installation/packaged-components#auto-deploying-manifests-addons
 mkdir -p /var/lib/rancher/k3s/server/manifests/
 cp /opt/k3s/manifests/* /var/lib/rancher/k3s/server/manifests/
 
