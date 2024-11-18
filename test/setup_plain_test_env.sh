2795064ed01920fc56d434a1d4f78821f00ca7cf
CoreyEWood <coreywood@outlook.com>
2024-11-18 12:19:27 -0800

    fix test env hopefully

--- test/setup_plain_test_env.sh
+++ test/setup_plain_test_env.sh
@@ -21,7 +21,7 @@
 # The following detector IDs correspond to the "dog" and "cat" detectors.
 
 EDGE_CONFIG=$(cat <<- EOM
-local_inference_templates:
+edge_inference_configs:
   default:
     enabled: true
   disabled:
@@ -29,10 +29,10 @@ local_inference_templates:
 
 detectors:
   - detector_id: 'det_2UOxalD1gegjk4TnyLbtGggiJ8p'
-    local_inference_template: 'disabled'
+    edge_inference_config: 'disabled'
 
   - detector_id: 'det_2UOxao4HZyB9gv4ZVtwMOvdqgh9'
-    local_inference_template: 'disabled'
+    edge_inference_config: 'disabled'
 EOM
 )
 
