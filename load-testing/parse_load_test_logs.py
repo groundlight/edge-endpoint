f9715345d12b1d65b61d4e68870f7ecf0cfc98ec
CoreyEWood <coreywood@outlook.com>
2024-12-20 14:52:08 -0800

    Load testing README updates and small improvements (#153)
    Updates the README to be accurate to the current configuration options.
    Adds a config variable to check the returned number of objects when
    using a count detector. Removes an unnecessary print statement.

--- load-testing/parse_load_test_logs.py
+++ load-testing/parse_load_test_logs.py
@@ -103,7 +103,6 @@ def plot_throughput_by_time(
     ax1.grid(True)
     ax1.legend(loc="upper left")
     # Set a fixed y-axis limit to avoid it adjusting based on the expected response rate
-    print(f"{max(*request_rate, *success_rate, *error_rate)=}")
     ax1.set_ylim((0, max(*request_rate, *success_rate, *error_rate) * 1.4))
 
     # Create a secondary y-axis for the number of clients
