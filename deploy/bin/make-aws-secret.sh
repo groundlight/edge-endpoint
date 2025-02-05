0d67ff68784bcbe13584ea4f3006a8cd8c1d42eb
Leo Dirac <leo@groundlight.ai>
2025-02-05 13:20:44 -0800

    Fixing the problem Corey found with AWS credentials.

--- deploy/bin/make-aws-secret.sh
+++ deploy/bin/make-aws-secret.sh
@@ -6,12 +6,24 @@ DEPLOYMENT_NAMESPACE=${DEPLOYMENT_NAMESPACE:-$($K config view -o json | jq -r '.
 K="$K -n $DEPLOYMENT_NAMESPACE"
 
 cd $(dirname "$0")
-# Make sure the initial login is in the correct namespace
+
+# Run the refresh-ecr-login.sh, telling it to use the configured KUBECTL_CMD
 KUBECTL_CMD="$K" ./refresh-ecr-login.sh
 
-# Use configured credentials if both are available, otherwise use environment variables
+# Now we try to find the AWS credentials.  Let's look in the CLI
+if command -v aws >/dev/null 2>&1; then
+    # Try to retrieve AWS credentials from aws configure
+    AWS_ACCESS_KEY_ID_CMD=$(aws configure get aws_access_key_id 2>/dev/null)
+    AWS_SECRET_ACCESS_KEY_CMD=$(aws configure get aws_secret_access_key 2>/dev/null)
+fi
+# Use the CLI credentials if available, otherwise use environment variables
 AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID_CMD:-$AWS_ACCESS_KEY_ID}
 AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY_CMD:-$AWS_SECRET_ACCESS_KEY}
+
+# Check that we have credentials
+if [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+    fail "No AWS credentials found"
+fi
 
 # Create the secret with either retrieved or environment values
 $K delete --ignore-not-found secret aws-credentials
