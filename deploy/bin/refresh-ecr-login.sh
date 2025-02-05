98dd65b81a8d87f0d613caaf2bea3d5df9fabac8
robotrapta <79607467+robotrapta@users.noreply.github.com>
2025-02-05 14:15:55 -0800

    Automatically refresh the ECR login credentials (#159)
    Cron-job that every 2 hours refreshes the ECR login token.
    
    I put all the needed functionality into the `edge-endpoint` container,
    which adds a few tools that weren't there before, so it's bigger. I also
    gave that container authz to create/destroy secrets. We could isolate
    this stuff more, but it would be more complex. The auth stuff is limited
    to the namespace.
    
    Tasks:
    - [x] Get the refresh script into Dockerfile
    - [x] Fully test
    
    ---------
    
    Co-authored-by: Ubuntu <ubuntu@ip-10-45-0-52.us-west-2.compute.internal>
    Co-authored-by: Tyler Romero <tyler@groundlight.ai>

---
+++ deploy/bin/refresh-ecr-login.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+K=${KUBECTL_CMD:-"kubectl"}
+# No need to explicitly pick the namespace - this normally runs in its own namespace
+
+ECR_REGISTRY="767397850842.dkr.ecr.us-west-2.amazonaws.com"
+# TODO: We should probably put this in DNS
+
+ECR_PASSWORD=$(aws ecr get-login-password --region us-west-2)
+if [ $? -ne 0 ]; then
+    echo "Failed to get ECR password"
+    exit 1
+fi
+
+echo "Fetched short-lived ECR credentials from AWS"
+
+if command -v docker >/dev/null 2>&1; then
+    echo $ECR_PASSWORD | docker login \
+        --username AWS \
+        --password-stdin  \
+        $ECR_REGISTRY
+else
+    echo "Docker is not installed. Skipping docker ECR login."
+fi
+
+$K delete --ignore-not-found secret registry-credentials
+
+$K create secret docker-registry registry-credentials \
+    --docker-server=$ECR_REGISTRY \
+    --docker-username=AWS \
+    --docker-password=$ECR_PASSWORD
+
+echo "Stored ECR credentials in secret registry-credentials"
