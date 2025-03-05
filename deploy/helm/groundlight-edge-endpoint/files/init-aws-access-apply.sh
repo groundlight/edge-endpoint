4e74c4d5bdd66b5cc8e19a2f9caf4398fc8f632f
Tom Faulhaber <tom@groundlight.ai>
2025-03-05 20:42:05 +0000

    More robust waiting for credentials to be ready

--- deploy/helm/groundlight-edge-endpoint/files/init-aws-access-apply.sh
+++ deploy/helm/groundlight-edge-endpoint/files/init-aws-access-apply.sh
@@ -8,7 +8,31 @@
 #    so that we can change it without restarting the pod.
 # 2. Create a secret with a Docker registry credentials. This is used to pull images from ECR.
 
-sleep 5  # Ensure the token is written before using it
+# We wait for the credentials to be written to the shared volume by the previous script.
+TIMEOUT=60  # Maximum time to wait in seconds
+FILE="/shared/done"
+
+echo "Waiting up to $TIMEOUT seconds for $FILE to exist..."
+
+i=0
+while [ $i -lt $TIMEOUT ]; do
+    if [ -f "$FILE" ]; then
+        echo "✅ File $FILE found! Continuing..."
+        break
+    fi
+    sleep 1
+    i=$((i + 1))
+done
+
+# If the loop completed without breaking, the file did not appear
+if [ ! -f "$FILE" ]; then
+    echo "❌ Error: File $FILE did not appear within $TIMEOUT seconds." >&2
+    exit 1
+fi
+
+
+echo "Creating Kubernetes secrets..."
+
 kubectl create secret generic aws-credentials-file --from-file /shared/credentials \
     --dry-run=client -o yaml | kubectl apply -f -
 
