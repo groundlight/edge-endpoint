c22705f5d9d76ba3389cc6b4cb0153c2cc3c88f8
Leo Dirac <leo@groundlight.ai>
2025-03-18 18:09:37 -0700

    Fixing some bugs in status-monitor.

--- app/status_monitor/status_web.py
+++ app/status_monitor/status_web.py
@@ -9,6 +9,7 @@ from fastapi.responses import HTMLResponse
 from app.metrics.metricreporting import report_metrics_to_cloud, metrics_payload
 
 LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO").upper()
+STATUS_REPORT_INTERVAL = int(os.environ.get("STATUS_REPORT_INTERVAL", 3600))
 
 logging.basicConfig(
     level=LOG_LEVEL, format="%(asctime)s.%(msecs)03d %(levelname)s %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
@@ -22,7 +23,9 @@ scheduler = AsyncIOScheduler()
 async def startup_event():
     """Lifecycle event that is triggered when the application starts."""
     logging.info("Starting status-monitor server...")
-    scheduler.add_job(report_metrics_to_cloud, "interval", seconds=3600)
+    scheduler.add_job(report_metrics_to_cloud, "interval", seconds=STATUS_REPORT_INTERVAL)
+    logging.info("Will report metrics to cloud every %d seconds", STATUS_REPORT_INTERVAL)
+    scheduler.start()
 
 @app.get("/metrics.json")
 async def get_metrics():
