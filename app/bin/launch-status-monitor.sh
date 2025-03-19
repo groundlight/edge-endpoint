4300b28a33d5f796d6418e62b8ac6b00a38e859a
Leo Dirac <leo@groundlight.ai>
2025-03-18 18:09:37 -0700

    Fixing some bugs in status-monitor.

--- app/bin/launch-status-monitor.sh
+++ app/bin/launch-status-monitor.sh
@@ -3,8 +3,8 @@
 cd $(dirname $0)  # script is in app/bin/ dir
 cd ../..
 
-poetry run gunicorn \
-    --workers 2 \
+poetry run uvicorn \
+    --workers 1 \
     --host 0.0.0.0 \
     --port 8123 \
     app.status_monitor.status_web:app
