7661fe2083983d9ebab7c5352bba622a94f2036b
Tom Faulhaber <tom@groundlight.ai>
2025-03-28 19:55:44 +0000

    Unified k3s install script (no gpu yet)

--- deploy/bin/install-k3s.sh
+++ deploy/bin/install-k3s.sh
@@ -1,15 +1,44 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-set -ex
+set -e
 
 K="k3s kubectl"
 
+echo '##################################################################################'
+echo '# Starting installation: updating system and installing dependencies'
+echo '##################################################################################'
 # Update system
 sudo apt update && sudo apt upgrade -y && sudo apt install -y jq curl
 
-# Check cgroup setup
-./check_cgroup.sh
+echo
+echo '##################################################################################'
+echo '# Check that cgroups are set up in the OS in a way that supports k3s'
+echo '##################################################################################'
+
+# Function to check if memory cgroups are actually working
+check_cgroup() {
+    if mount | grep -q "cgroup2"; then
+        echo "Cgroup v2 is enabled."
+        return 0
+    elif mount | grep -q "cgroup/memory"; then
+        # For cgroup v1, verify memory controller is actually functional
+        if [ -f "/sys/fs/cgroup/memory/memory.limit_in_bytes" ]; then
+            echo "Cgroup v1 (memory) is enabled and functional."
+            return 0
+        else
+            echo "Cgroup v1 memory controller found but may not be functional."
+            echo "You may need to add 'cgroup_memory=1 cgroup_enable=memory' to kernel command line."
+            return 1
+        fi
+    else
+        echo "No memory cgroup found."
+        echo "You may need to add 'cgroup_memory=1 cgroup_enable=memory' to kernel command line."
+        return 1
+    fi
+}
+
+check_cgroup
 CGROUP_STATUS=$?
 
 if [ $CGROUP_STATUS -eq 1 ]; then
@@ -23,8 +52,11 @@ EOF
     exit 1
 fi
 
-# Install k3s
-echo "Installing k3s..."
+echo
+echo '##################################################################################'
+echo '# Installing k3s'
+echo '##################################################################################'
+
 curl -sfL https://get.k3s.io |  K3S_KUBECONFIG_MODE="644" sh -s - --disable=traefik
 
 check_k3s_is_running() {
@@ -50,14 +82,71 @@ else
     exit 0
 fi
 
-# Set up kubeconfig for the current user
-./add-k3s-cluster-to-config.sh
+echo
+echo '##################################################################################'
+echo '# Setting up kubeconfig for the current user'
+echo '##################################################################################'
 
-echo "You might want to set KUBECONFIG as follows:"
-echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml"
+# Copy the cluster information defined in the k3s config file to the user's kubeconfig file
+# If the user already has a kubeconfig file, add the cluster information to the existing file
+# otherwise create a new kubeconfig file
 
-# Install helm
-echo "Installing helm..."
+add_kubectl_config() {
+    # Don't use `k3s kubectl` here because it always uses the default kubeconfig file in /etc/rancher/k3s/k3s.yaml
+    local K=${KUBECTL_CMD:-kubectl}
+
+
+    local k3scfg="/etc/rancher/k3s/k3s.yaml"
+
+    local kubectl_is_k3s=$($K version --client | egrep "^Client Version.*+k3s" || true)
+
+    if [ ! -f $k3scfg ]; then
+        echo "ERROR: k3s config file ('${k3scfg}') not found"
+        exit 1
+    fi
+
+    export KUBECONFIG=$HOME/.kube/config
+
+    if [ -n "$kubectl_is_k3s" ]; then
+        echo "Using the kubectl supplied by k3s. Shared kubeconfig file is at ${k3scfg}"
+        echo "We'll copy that info to the user's kubeconfig file so that Helm can find it."
+        
+        if [ -f $HOME/.kube/config ]; then
+            echo "User's kubeconfig file already exists. Adding cluster information to the existing file."
+        else
+            echo "User's kubeconfig file does not exist. Creating a new kubeconfig file."
+            mkdir -p $HOME/.kube
+            $K config view --kubeconfig=${k3scfg} --minify --flatten --output 'jsonpath={.clusters[?(@.name=="default")].cluster}' > $HOME/.kube/config
+        fi
+    fi
+
+    # Get the cluster and user information from the k3s config file
+    server=$($K config view --kubeconfig="${k3scfg}" --minify --flatten --output 'jsonpath={.clusters[?(@.name=="default")].cluster.server}')
+    cert=$($K config view --kubeconfig="${k3scfg}" --minify --flatten --output 'jsonpath={.clusters[?(@.name=="default")].cluster.certificate-authority-data}')
+
+    user_client_certificate=$(kubectl config view --kubeconfig=${k3scfg} --minify --flatten --output 'jsonpath={.users[?(@.name=="default")].user.client-certificate-data}')
+    user_client_key=$(kubectl config view --kubeconfig=${k3scfg} --minify --flatten --output 'jsonpath={.users[?(@.name=="default")].user.client-key-data}')
+
+    # Add the cluster information to the user's kubeconfig file
+    $K config set-cluster k3s --server=$server --certificate-authority=<(echo "${cert}" | base64 -d) --embed-certs=true
+    $K config set-credentials k3s \
+    --embed-certs=true \
+    --client-certificate=<(echo "${user_client_certificate}" | base64 -d) \
+    --client-key=<(echo "${user_client_key}" | base64 -d)
+
+    # create a context that points to the default namespace in the new cluster
+    $K config set-context k3s --cluster=k3s --user=k3s
+    $K config use-context k3s
+}
+
+add_kubectl_config
+echo "kubectl has been configured for the current user."
+
+echo
+echo '##################################################################################'
+echo '# Installing helm and connecting to our helm repo'
+echo '##################################################################################'
+
 curl -fsSL -o /tmp/get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod 700 /tmp/get_helm.sh
 /tmp/get_helm.sh
@@ -65,5 +154,10 @@ chmod 700 /tmp/get_helm.sh
 # Add the edge-endpoint helm repo
 helm repo add edge-endpoint https://code.groundlight.ai/edge-endpoint/
 helm repo update
+
+echo
+echo '##################################################################################'
+echo '# Installation successful'
+echo '##################################################################################'
 
 
