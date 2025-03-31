ee7685c97e5a3a79f87726e6d29a7c8e90c080dc
Tom Faulhaber <tom@groundlight.ai>
2025-03-31 16:31:38 +0000

    Add GPU support to the base script

--- deploy/bin/install-k3s.sh
+++ deploy/bin/install-k3s.sh
@@ -5,6 +5,26 @@ set -e
 
 K="k3s kubectl"
 
+#!/bin/bash
+
+# Validate number of arguments
+if [ "$#" -ne 1 ]; then
+    echo "❌ Error: Exactly one argument is required: 'gpu' or 'cpu'." >&2
+    exit 1
+fi
+
+# Convert to lowercase
+FLAVOR=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+
+# Validate value
+if [ "$FLAVOR" != "gpu" ] && [ "$FLAVOR" != "cpu" ]; then
+    echo "❌ Error: Argument must be either 'gpu' or 'cpu'." >&2
+    exit 1
+fi
+
+# Confirm success
+echo "Flavor set to: $FLAVOR"
+
 echo '##################################################################################'
 echo '# Starting installation: updating system and installing dependencies'
 echo '##################################################################################'
@@ -50,6 +70,57 @@ You can do this with grub on most systems,
 or on Raspberry Pi by editing /boot/cmdline.txt or /boot/firmware/cmdline.txt.
 EOF
     exit 1
+fi
+
+# Install and configure GPU support, if requested
+# Tested on an AWS EC2 G4 instance using the following AMI:
+# Deep Learning OSS Nvidia Driver AMI GPU PyTorch 2.3.0 (Ubuntu 20.04) 20240825
+
+# This guide was more helpful than others fwiw:
+# https://support.tools/post/nvidia-gpus-on-k3s/
+
+
+check_nvidia_drivers_and_container_runtime() {
+  # Retrieve existing version or default to 525
+  NVIDIA_VERSION=$(modinfo nvidia 2>/dev/null | awk '/^version:/ {split($2, a, "."); print a[1]}')
+  NVIDIA_VERSION=${NVIDIA_VERSION:-525}
+
+  if ! command -v nvidia-smi &> /dev/null; then
+    echo "NVIDIA drivers are not installed (nvidia-smi not found). Installing..."
+    sudo apt update && sudo apt install -y "nvidia-headless-$NVIDIA_VERSION-server" "nvidia-utils-$NVIDIA_VERSION-server"
+  else
+    echo "NVIDIA drivers for version $NVIDIA_VERSION are installed."
+  fi
+
+  # Check if nvidia container runtime is already installed.
+  if ! command -v nvidia-container-runtime &> /dev/null; then
+    echo " NVIDIA container runtime is not installed. Installing..."
+    # Get distribution information
+    DISTRIBUTION=$(. /etc/os-release; echo "$ID$VERSION_ID")
+
+    if ! command -v curl &> /dev/null; then
+      echo "Installing curl to retrieve NVIDIA repository info"
+      sudo apt update -y && sudo apt install -y curl
+    fi
+
+    # Add NVIDIA Docker repository
+    echo "Adding NVIDIA Docker repository..."
+    curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+    curl -s -L "https://nvidia.github.io/nvidia-docker/$DISTRIBUTION/nvidia-docker.list" | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+    sudo apt update -y && sudo apt install -y nvidia-container-runtime
+  else
+    echo " NVIDIA container runtime is installed."
+  fi
+}
+
+if [ "$FLAVOR" == "gpu" ]; then
+    echo
+    echo '##################################################################################'
+    echo '# Installing driver support for NVIDIA GPUs'
+    echo '##################################################################################'
+    check_nvidia_drivers_and_container_runtime
+    echo "NVIDIA GPU support installed."
 fi
 
 echo
@@ -154,6 +225,68 @@ chmod 700 /tmp/get_helm.sh
 # Add the edge-endpoint helm repo
 helm repo add edge-endpoint https://code.groundlight.ai/edge-endpoint/
 helm repo update
+
+wait_for_gpu() {
+    # Verify that we actually added GPU capacity to the node
+    local capacity=0
+    local elapsed=0
+    # Even 5 minutes isn't enough sometimes.
+    local timeout_min=10
+
+    echo
+    echo "Waiting up to $timeout_min minutes for the GPU capacity to come online:"
+
+    timeout_sec=$((timeout_min * 60))
+    while [ "$elapsed" -lt "$timeout_sec" ]; do
+        node_name=$($K get nodes -o name | head -n1)
+        # First make sure the node has registered
+        if [ -z "$node_name" ]; then
+            echo -n "Waiting for node to register..."
+            sleep 1
+            ((elapsed++)) || true
+            continue
+        fi
+
+        # Now check the GPU capacity for the node
+        capacity=$($K get "$node_name" -o=jsonpath='{.status.capacity.nvidia\.com/gpu}')
+
+        # Check if GPU capacity is non-zero
+        if [ -n "$capacity" ] && [ "$capacity" -ne 0 ]; then
+            break
+        fi
+
+        echo -n "."
+
+        # Wait for 1 second
+        sleep 1
+        ((elapsed++)) || true
+    done
+
+    echo
+    if [ "$capacity" -gt 0 ]; then
+        echo "GPU capacity successfully added"
+    else
+        echo "WARNING: k3s sees no GPU capacity on node after install!!"
+        if ! nvidia-smi &> /dev/null; then
+            echo "Running nvidia-smi failed, so NVIDIA drivers are probably not working."
+            echo "Rebooting might help."
+        fi
+        exit 1
+    fi
+}
+
+if [ "$FLAVOR" == "gpu" ]; then
+    echo
+    echo '##################################################################################'
+    echo '# Waiting for GPU capacity to come online'
+    echo '##################################################################################'
+    wait_for_gpu
+fi
+
+# In addition, you can also check that the nvidia-device-plugin-ds pod
+# is running in the `kube-system` namespace.
+# kubectl get pods -n kube-system -l name=nvidia-device-plugin-ds
+
 
 echo
 echo '##################################################################################'
