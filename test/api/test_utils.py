93881359b583c96fa17fd4c61b84d260c61079ef
CoreyEWood <coreywood@outlook.com>
2025-04-02 16:41:27 -0700

    Fix tests and add new one for IQ not done processing

--- test/api/test_utils.py
+++ test/api/test_utils.py
@@ -31,6 +31,7 @@ class TestCreateIQ:
             result_value=0,
             confidence=0.8,
             confidence_threshold=self.confidence_threshold,
+            is_done_processing=True,
             query="Test query",
         )
 
@@ -39,8 +40,25 @@ class TestCreateIQ:
         assert isinstance(iq.result, BinaryClassificationResult)
         assert iq.result.source == Source.ALGORITHM
         assert iq.result.label == Label.YES
+        assert iq.result.from_edge
         assert "is_from_edge" in iq.metadata
         assert iq.metadata["is_from_edge"]
+        assert iq.done_processing
+
+    def test_create_binary_iq_not_done_processing(self):
+        """Test creating a basic binary IQ."""
+        iq = create_iq(
+            detector_id=prefixed_ksuid("det_"),
+            mode=ModeEnum.BINARY,
+            mode_configuration=None,
+            result_value=0,
+            confidence=0.8,
+            confidence_threshold=self.confidence_threshold,
+            is_done_processing=False,  # Not done processing
+            query="Test query",
+        )
+
+        assert not iq.done_processing
 
     def test_create_count_iq(self):
         """Test creating a basic count IQ."""
@@ -52,6 +70,7 @@ class TestCreateIQ:
             result_value=count_value,
             confidence=0.8,
             confidence_threshold=self.confidence_threshold,
+            is_done_processing=True,
             query="Test query",
         )
 
@@ -61,8 +80,10 @@ class TestCreateIQ:
         assert iq.result.source == Source.ALGORITHM
         assert iq.result.count == count_value
         assert not iq.result.greater_than_max
+        assert iq.result.from_edge
         assert "is_from_edge" in iq.metadata
         assert iq.metadata["is_from_edge"]
+        assert iq.done_processing
 
     def test_create_count_iq_greater_than_max(self):
         """Test creating a count IQ with count greater than the max count."""
@@ -75,6 +96,7 @@ class TestCreateIQ:
             result_value=count_value,
             confidence=0.8,
             confidence_threshold=self.confidence_threshold,
+            is_done_processing=True,
             query="Test query",
         )
 
@@ -83,6 +105,8 @@ class TestCreateIQ:
         assert iq.result.source == Source.ALGORITHM
         assert iq.result.greater_than_max
         assert iq.result.count == max_count_value
+        assert iq.result.from_edge
+        assert iq.done_processing
 
     def test_create_multiclass_iq(self):
         """Test creating a basic multiclass IQ."""
@@ -97,6 +121,7 @@ class TestCreateIQ:
                 result_value=1,
                 confidence=0.8,
                 confidence_threshold=self.confidence_threshold,
+                is_done_processing=True,
                 query="Test query",
             )
 
@@ -110,6 +135,7 @@ class TestCreateIQ:
                 result_value=1,
                 confidence=0.8,
                 confidence_threshold=self.confidence_threshold,
+                is_done_processing=True,
                 query="Test query",
             )
 
