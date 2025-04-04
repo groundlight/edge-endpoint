c6bfa02957f31b21fb5d3eb8de7765b3b837e0f7
f-wright <francine@groundlight.ai>
2025-04-04 00:01:18 +0000

    fix iq activity tests

--- test/metrics/test_iqactivity.py
+++ test/metrics/test_iqactivity.py
@@ -18,7 +18,7 @@ def _test_tracker(tmp_base_dir):
     yield tracker
 
 
-def test_initial_directories(tmp_base_dir):
+def test_initial_directories(tmp_base_dir, _test_tracker):
     # Check that basic directories exist after we create the tracker
     assert Path(tmp_base_dir).exists()
     assert Path(tmp_base_dir, "detectors").exists()
@@ -26,48 +26,44 @@ def test_initial_directories(tmp_base_dir):
 
 def test_increment_counter_file(tmp_base_dir, _test_tracker):
     assert not Path(tmp_base_dir, "increment_test").exists()
-    _test_tracker().increment_counter_file("increment_test")
+    _test_tracker.increment_counter_file("increment_test")
     assert Path(tmp_base_dir, "increment_test").exists()
     assert Path(tmp_base_dir, "increment_test").read_text() == "1"
-    _test_tracker().increment_counter_file("increment_test")
+    _test_tracker.increment_counter_file("increment_test")
     assert Path(tmp_base_dir, "increment_test").read_text() == "2"
 
     assert not Path(tmp_base_dir, "detectors", "det_incrementtest", "increment_test").exists()
-    _test_tracker().increment_counter_file("increment_test", "det_incrementtest")
+    _test_tracker.increment_counter_file("increment_test", "det_incrementtest")
     assert Path(tmp_base_dir, "detectors", "det_incrementtest", "increment_test").exists()
     assert Path(tmp_base_dir, "detectors", "det_incrementtest", "increment_test").read_text() == "1"
-    _test_tracker().increment_counter_file("increment_test", "det_incrementtest")
+    _test_tracker.increment_counter_file("increment_test", "det_incrementtest")
     assert Path(tmp_base_dir, "detectors", "det_incrementtest", "increment_test").read_text() == "2"
 
 
-def test_activity_tracking(tmp_base_dir, _test_tracker):
+def test_activity_tracking(monkeypatch, tmp_base_dir, _test_tracker):
+    monkeypatch.setattr("app.metrics.iqactivity._tracker", lambda: _test_tracker)
     assert not Path(tmp_base_dir, "detectors", "det_recordactivitytest").exists()
 
     # Record an IQ, check that the last_iq file, iqs file, and detector-specific iqs file are all
     # created and have the correct values
-    with patch("app.metrics.iqactivity.datetime.now") as mock_now:
-        mock_now.return_value = datetime(2025, 4, 3, 12, 0, 0)
+    with patch("app.metrics.iqactivity.datetime") as mock_datetime:
+        mock_datetime.now.return_value = datetime(2025, 4, 3, 12, 0, 0)
         record_activity("det_recordactivitytest", "iqs")
         assert Path(tmp_base_dir, "detectors", "det_recordactivitytest", "iqs").exists()
         assert Path(tmp_base_dir, "detectors", "det_recordactivitytest", "iqs").read_text() == "1"
         assert Path(tmp_base_dir, "detectors", "det_recordactivitytest", "iqs_2025-04-03_12").exists()
         assert Path(tmp_base_dir, "detectors", "det_recordactivitytest", "iqs_2025-04-03_12").read_text() == "1"
         assert Path(tmp_base_dir, "last_iq").exists()
-        first_iq_time = _test_tracker.get_last_file_activity("last_iq")
-        assert first_iq_time is not None
         assert Path(tmp_base_dir, "iqs").exists()
         assert Path(tmp_base_dir, "iqs").read_text() == "1"
         assert Path(tmp_base_dir, "iqs_2025-04-03_12").exists()
         assert Path(tmp_base_dir, "iqs_2025-04-03_12").read_text() == "1"
 
         # Record another IQ, make sure all files are updated correctly
-        mock_now.return_value = datetime(2025, 4, 3, 12, 0, 1)
+        mock_datetime.now.return_value = datetime(2025, 4, 3, 12, 0, 1)
         record_activity("det_recordactivitytest", "iqs")
         assert Path(tmp_base_dir, "detectors", "det_recordactivitytest", "iqs").read_text() == "2"
         assert Path(tmp_base_dir, "detectors", "det_recordactivitytest", "iqs_2025-04-03_12").read_text() == "2"
-        second_iq_time = _test_tracker.get_last_file_activity("last_iq")
-        assert second_iq_time is not None
-        assert second_iq_time > first_iq_time
         assert Path(tmp_base_dir, "iqs").read_text() == "2"
         assert Path(tmp_base_dir, "detectors", "det_recordactivitytest", "iqs").read_text() == "2"
         assert Path(tmp_base_dir, "iqs_2025-04-03_12").read_text() == "2"
@@ -86,12 +82,13 @@ def test_activity_tracking(tmp_base_dir, _test_tracker):
         assert Path(tmp_base_dir, "detectors", "det_recordactivitytest", "audits_2025-04-03_12").read_text() == "1"
 
 
-def test_wrong_activity_type():
-    with pytest.raises(ValueError):
-        record_activity("det_123", "wrong_activity_type")
+def test_wrong_activity_type(caplog):
+    record_activity("det_123", "wrong_activity_type")
+    assert "The provided activity type (wrong_activity_type) is not currently supported" in caplog.text
 
 
-def test_clear_old_activity_files(tmp_base_dir):
+def test_clear_old_activity_files(monkeypatch, tmp_base_dir, _test_tracker):
+    monkeypatch.setattr("app.metrics.iqactivity._tracker", lambda: _test_tracker)
     Path(tmp_base_dir, "detectors", "det_123").mkdir(parents=True)
     Path(tmp_base_dir, "detectors", "det_456").mkdir(parents=True)
 
@@ -120,8 +117,8 @@ def test_clear_old_activity_files(tmp_base_dir):
     Path(tmp_base_dir, "detectors", "det_456", "escalations").touch()
     Path(tmp_base_dir, "detectors", "det_456", "iqs_2010-04-03_09").touch()
 
-    with patch("app.metrics.iqactivity.datetime.now") as mock_now:
-        mock_now.return_value = datetime(2025, 4, 3, 12, 30, 0)
+    with patch("app.metrics.iqactivity.datetime") as mock_datetime:
+        mock_datetime.now.return_value = datetime(2025, 4, 3, 12, 30, 0)
         clear_old_activity_files()
 
     # All totals files created should be kept
@@ -143,7 +140,7 @@ def test_clear_old_activity_files(tmp_base_dir):
     assert Path(tmp_base_dir, "detectors", "det_123", "audits_2025-04-03_12").exists()
 
     # All hourly files outside of the last 3 hours should be deleted
-    assert not Path(tmp_base_dir, "iqs_2025-04-03_08").exists()
+    assert not Path(tmp_base_dir, "iqs_2025-04-03_08").exists(), f"all files in {tmp_base_dir} are {list(Path(tmp_base_dir).iterdir())}"
     assert not Path(tmp_base_dir, "iqs_2025-04-03_09").exists()
     assert not Path(tmp_base_dir, "detectors", "det_123", "iqs_2025-04-03_08").exists()
     assert not Path(tmp_base_dir, "detectors", "det_123", "iqs_2025-04-03_09").exists()
