9e82acbd786f2b44abae8d4d7ccacb939c65e729
CoreyEWood <coreywood@outlook.com>
2025-05-01 11:16:20 -0700

    send to cloud at end for debugging

--- test/integration/integration.py
+++ test/integration/integration.py
@@ -82,12 +82,12 @@ def submit_initial(detector: Detector) -> str:
     # a bit dependent on the current default model,
     # but that one always defaults to 0.5 confidence at first.
 
-    assert (
-        0.5 <= iq_yes.result.confidence <= 0.55
-    ), f"Expected confidence to be between 0.5 and 0.55, but got {iq_yes.result.confidence}"
-    assert (
-        0.5 <= iq_no.result.confidence <= 0.55
-    ), f"Expected confidence to be between 0.5 and 0.55, but got {iq_no.result.confidence}"
+    assert 0.5 <= iq_yes.result.confidence <= 0.55, (
+        f"Expected confidence to be between 0.5 and 0.55, but got {iq_yes.result.confidence}"
+    )
+    assert 0.5 <= iq_no.result.confidence <= 0.55, (
+        f"Expected confidence to be between 0.5 and 0.55, but got {iq_no.result.confidence}"
+    )
 
 
 def improve_model(detector: Detector):
@@ -112,25 +112,29 @@ def submit_final(detector: Detector):
     end_time = time.time()
     print(f"Time taken to get high confidence response from edge: {end_time - start_time} seconds")
 
-    yes_result_string = (
-        f"For the final cat/YES image, received answer {iq_yes.result.label} with confidence "
-        f"{iq_yes.result.confidence}. Metadata: {iq_yes.metadata}."
+    iq_yes_cloud = _submit_cat(detector, confidence_threshold=1, wait=0)
+    iq_no_cloud = _submit_dog(detector, confidence_threshold=1, wait=0)
+    cloud_yes_result_string = (
+        f"For the final cat/YES image sent to the cloud ({iq_yes_cloud.id}), received answer "
+        f"{iq_yes_cloud.result.label} with confidence {iq_yes_cloud.result.confidence}. "
+        f"Metadata: {iq_yes_cloud.metadata}."
     )
-    print(yes_result_string)
-    no_result_string = (
-        f"For the final dog/NO image, received answer {iq_no.result.label} with confidence "
-        f"{iq_no.result.confidence}. Metadata: {iq_no.metadata}."
+    print(cloud_yes_result_string)
+    cloud_no_result_string = (
+        f"For the final dog/NO image sent to the cloud ({iq_no_cloud.id}), received answer "
+        f"{iq_no_cloud.result.label} with confidence {iq_no_cloud.result.confidence}. "
+        f"Metadata: {iq_no_cloud.metadata}."
     )
-    print(no_result_string)
+    print(cloud_no_result_string)
 
-    assert (
-        iq_yes.result.confidence > ACCEPTABLE_TRAINED_CONFIDENCE
-    ), f"Expected confidence to be greater than {ACCEPTABLE_TRAINED_CONFIDENCE}, but got {iq_yes.result.confidence}"
+    assert iq_yes.result.confidence > ACCEPTABLE_TRAINED_CONFIDENCE, (
+        f"Expected confidence to be greater than {ACCEPTABLE_TRAINED_CONFIDENCE}, but got {iq_yes.result.confidence}"
+    )
     assert iq_yes.result.label.value == "YES", f"Expected label to be YES, but got {iq_yes.result.label.value}"
 
-    assert (
-        iq_no.result.confidence > ACCEPTABLE_TRAINED_CONFIDENCE
-    ), f"Expected confidence to be greater than {ACCEPTABLE_TRAINED_CONFIDENCE}, but got {iq_no.result.confidence}"
+    assert iq_no.result.confidence > ACCEPTABLE_TRAINED_CONFIDENCE, (
+        f"Expected confidence to be greater than {ACCEPTABLE_TRAINED_CONFIDENCE}, but got {iq_no.result.confidence}"
+    )
     assert iq_no.result.label.value == "NO", f"Expected label to be NO, but got {iq_no.result.label.value}"
 
 
