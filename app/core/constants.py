b7c563180364598eba3ba3d8cfa5bfe7fd670732
CoreyEWood <coreywood@outlook.com>
2025-05-07 13:11:27 -0700

    change ttl time and check for connection when fetching models too

--- app/core/constants.py
+++ app/core/constants.py
@@ -2,7 +2,7 @@ DEFAULT_PATIENCE_TIME: int = 30  # Default patience time in seconds
 
 CONNECTION_TEST_HOST, CONNECTION_TEST_PORT = "8.8.8.8", 53  # Google's DNS server
 CONNECTION_TIMEOUT = 1  # Number of seconds to attempt to connect
-CONNECTION_STATUS_TTL_SECS = 1  # Number of seconds to cache the connection status
+CONNECTION_STATUS_TTL_SECS = 0.5  # Number of seconds to cache the connection status
 
 DEFAULT_POLLING_INITIAL_DELAY = 0.25
 DEFAULT_POLLING_EXPONENTIAL_BACKOFF = 1.3
