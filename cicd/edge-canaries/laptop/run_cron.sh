51c783fa2c7e6863dc2c571744e7aa6a7a0408fc
Tim Huff <89954856+timmarkhuff@users.noreply.github.com>
2025-06-04 22:45:51 +0000

    Adding edge canary on laptop (#182)
    This PR adds an Edge Endpoint canary that runs on a laptop.
    
    Completed so far:
    1. a script that installs a cron job to periodically runs a load test
    and check he result
    2. The script creates a heartbeat detector and a Groundlight alert for
    that detector, if it doesn't already exist. The idea is at the end of
    every successful load test, the script will submit one image query to
    the heartbeat detector. It will be submitted to the cloud, not edge. If
    the heartbeat detector doesn't receive an image query for a given period
    of time, Groundlight will send an email alert.
    3. This canary writes local logs like this:
    ```
    Cron job started at Fri Feb 21 05:05:01 PM PST 2025
    2025-02-21 17:05:02 - INFO - Heartbeat alert already exists. Skipping...
    2025-02-21 17:05:04 - INFO - Starting laptop edge canary test. Submitting 1000 image queries to det_2sxWe4bhmSjcAeqNk2R9wc2xaIb...
    2025-02-21 17:08:00 - INFO - Processed 1000 image queries in 175.91 seconds at 5.68 queries per second.
    2025-02-21 17:08:00 - INFO - Finished with an average confidence of 0.95.
    2025-02-21 17:08:00 - INFO - Laptop edge canary seems to be online and functioning properly. Submitting heartbeat...
    2025-02-21 17:08:01 - INFO - Heartbeat submitted!
    Cron job finished at Fri Feb 21 05:08:01 PM PST 2025
    ```
    
    What remains to be done:
    1. Figure out how `edge-endpoint` picks up new docker images. Is this
    happening automatically? Probably need to add some GHA that uploads
    images with a `canaries` or `staging` tag prior to pushing to `latest`.
    Maybe that should be a separate PR?

---
+++ cicd/edge-canaries/laptop/run_cron.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Get the absolute path of the laptop directory (where this script is located)
+BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+VENV_PATH="$BASE_DIR/.venv"
+PYTHON_SCRIPT="$BASE_DIR/run.py"
+LOG_FILE="$HOME/canary_logs/laptop_canary.log"
+ENV_FILE="$BASE_DIR/.env"
+
+# Load environment variables from .env
+if [ -f "$ENV_FILE" ]; then
+    export $(grep -v '^#' "$ENV_FILE" | xargs)
+else
+    echo "ERROR: .env file not found! Exiting..." >> "$LOG_FILE" 2>&1
+    exit 1
+fi
+
+# Activate virtual environment
+source "$VENV_PATH/bin/activate"
+
+# Make sure log file exists
+mkdir -p "$(dirname "$LOG_FILE")" && touch "$LOG_FILE"
+
+# Log execution info (for debugging)
+echo "----------------------------" >> "$LOG_FILE" 2>&1
+echo "Cron job started at $(date)" >> "$LOG_FILE" 2>&1
+
+# Run the python script and log output to LOG_FILE
+# Redirect any logs from OpenCV that contain "[h264 @" because they will clutter the logs
+python3 "$PYTHON_SCRIPT" 2> >(stdbuf -oL grep -vE "\[h264 @" >> "$LOG_FILE") >> "$LOG_FILE"
+
+echo "Cron job finished at $(date)" >> "$LOG_FILE" 2>&1
