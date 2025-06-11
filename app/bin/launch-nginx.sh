fc8a38c414c4b38a732a5eff86b9805aca2c2a79
Tom Faulhaber <tom@groundlight.ai>
2025-06-11 17:36:10 +0000

    Fixes to comments

--- app/bin/launch-nginx.sh
+++ app/bin/launch-nginx.sh
@@ -2,7 +2,7 @@
 
 # Nginx won't refresh the IP address of the upstream endpoint without the "resolver" directive
 # but that requires the specific IP address of the DNS server, which can be different in different
-# clusters, so we determing that at runtime and edit nginx.conf before starting nginx.
+# clusters, so we determine that at runtime and edit nginx.conf before starting nginx.
 
 NAME_SERVER=$(awk '/^nameserver / {print $2; exit}' /etc/resolv.conf)
 echo "Using nameserver: $NAME_SERVER"
