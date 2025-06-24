3f9f9ea7a8efaaf4c48e904ab4c2e5225570a3fe
CoreyEWood <coreywood@outlook.com>
2025-06-24 16:50:11 -0700

    remove unneeded constant

--- app/escalation_queue/constants.py
+++ app/escalation_queue/constants.py
@@ -4,4 +4,3 @@ WRITING_DIR_SUFFIX = "writing"
 IMAGE_DIR_SUFFIX = "images"
 TRACKING_FILE_NAME_PREFIX = "tracking-"  # Prefix for naming tracking files
 MAX_QUEUE_FILE_LINES = 200  # Maximum number of lines written to each escalation queue file.
-MAX_RETRY_ATTEMPTS = 3  # Maximum number of times that an escalation will be attempted before giving up.
