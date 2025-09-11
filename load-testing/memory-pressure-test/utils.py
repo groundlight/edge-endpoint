84f301e8137e57b25ce287bf3476d9a0cd7898d9
Tim Huff <89954856+timmarkhuff@users.noreply.github.com>
2025-09-11 15:37:46 -0700

    Distributing inference pods across available GPUs (#270)
    This PR allows machines with multiple GPUs to utilize all of their GPUs.
    
    I have updated the inference image to contain a script for selecting a
    GPU, if there are multiple GPUs.
    
    I also refactored the memory pressure script so that it could be used
    for the sort of testing I was doing on this PR.

--- load-testing/memory-pressure-test/utils.py
+++ load-testing/memory-pressure-test/utils.py
@@ -1,39 +1,45 @@
 import numpy as np
 import random
 import groundlight
-from groundlight import ROI
+from groundlight import ROI, Groundlight
 import math
 import cv2
 
 from datetime import datetime
 
-# We need to establish a client here so that we can use functions like `gl.create_roi`, but we won't
-# actually use it to submit anything to Groundlight
-gl = groundlight.Groundlight(endpoint=None)
-
 IMAGE_DIMENSIONS = (480, 640, 3)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
-def get_random_binary_image() -> tuple[np.ndarray, str]:
+def generate_random_binary_image(
+    gl: Groundlight,  # not used, but added here to maintain consistency with `generate_random_count_image`
+    image_width: int = 640,
+    image_height: int = 480,
+) -> tuple[np.ndarray, str]:
     """
     Used for generating random data to submit to Groundlight for load testing.
-     
-    Randomly generates either a black or white image, with the datetime overlaid.
     
-    Returns the image and the corresponding label.
+    Randomly generates either a black or white image of the specified dimensions,
+    with the datetime overlaid.
+
+    Returns:
+        tuple: (image as np.ndarray, label as str)
     """
+    image_shape = (image_height, image_width, 3)
+
     if random.choice([True, False]):
-        image = np.zeros(IMAGE_DIMENSIONS, dtype=np.uint8)  # Black image
+        image = np.zeros(image_shape, dtype=np.uint8)  # Black image
         text_color = WHITE
         label = "YES"
     else:
-        image = np.full(IMAGE_DIMENSIONS, 255, dtype=np.uint8)  # White image
+        image = np.full(image_shape, 255, dtype=np.uint8)  # White image
         text_color = BLACK
         label = "NO"
+
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     cv2.putText(image, timestamp, (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, text_color, 2)
-    return image, label
+
+    return image, label, None # return rois as None to maintain consistency with `generate_random_count_image` 
 
 def get_random_color() -> tuple[int, int, int]:
     return tuple(int(x) for x in np.random.randint(0, 256, 3))
@@ -42,10 +48,11 @@ def generate_color_canvas(width: int, height: int, color: tuple[int, int, int]) 
     return np.full((height, width, 3), color, dtype=np.uint8)
 
 def generate_random_count_image(
-        class_name: str,
-        max_count: int = 10,
+        gl: Groundlight,
         image_width: int = 640,
         image_height: int = 480,
+        class_name: str = 'object',
+        max_count: int = 10,
     ) -> tuple[np.ndarray, list[ROI]]:
     """
     Used for generating random data to submit to Groundlight for load testing.
@@ -92,4 +99,6 @@ def generate_random_count_image(
         )
         rois.append(roi)
 
-    return image, rois
+    label = len(rois)
+
+    return image, label, rois
